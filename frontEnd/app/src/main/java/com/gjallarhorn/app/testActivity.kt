faade3e84832a1b196f70923d904ff09039a46c7
ajlarivi <andru.lariviere@gmail.com>
2019-07-29 21:18:19 -0700

    implemented random fetching from database

--- frontEnd/app/src/main/java/com/gjallarhorn/app/testActivity.kt
+++ frontEnd/app/src/main/java/com/gjallarhorn/app/testActivity.kt
@@ -2,18 +2,29 @@ package com.gjallarhorn.app
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 
 import kotlinx.android.synthetic.main.activity_test.*
 import java.io.File
 import java.io.FileInputStream
 import java.io.IOException
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import java.net.URL
+
 
 class testActivity : AppCompatActivity() {
     lateinit var storage: FirebaseStorage
+    lateinit var database: FirebaseFirestore
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -29,20 +40,39 @@ class testActivity : AppCompatActivity() {
 
     //retrieve an individual file from firebase
     fun individualPull(view: View){
+        val database = FirebaseFirestore.getInstance()
+
+        val alarmTitle = "disgust"
+        val dataRef = database.collection("Alarms")
+            //.whereEqualTo("AlarmName", alarmTitle)
+            .get()
+            .addOnSuccessListener{ documents ->
+                val listURL: MutableList<String> = mutableListOf<String>()
+                val listName: MutableList<String> = mutableListOf<String>()
+
+                for (document in documents){
+                    listURL.add(document.getString("URL").toString())
+                    listName.add(document.getString("AlarmName").toString())
+                    Log.d("Firebase: ", "${document.id} => ${document.data}")
+
+                }
+
+                val randNum = (0..listURL.size-1).random()
+                val storageURL = listURL.get(randNum)
+                val selectedName = listName.get(randNum)
+                Log.d("Firebase: ", storageURL)
+                pullFromURL(storageURL, selectedName)
+        }
+    }
+
+    fun pullFromURL(storageURL:String, name:String){
         val storage = FirebaseStorage.getInstance()
-        val storageRef = storage.reference
+        val httpsReference = storage.getReferenceFromUrl(storageURL)
         val mediaPlayer = MediaPlayer()
-        val fileName = "Yee.mp3"
 
-        val refPath = "Sounds/" + fileName
+        val localFile = File.createTempFile(name, "m4a")
 
-
-
-        val islandRef = storageRef.child(refPath)
-
-        val localFile = File.createTempFile("yee", "mp3")
-
-        islandRef.getFile(localFile).addOnSuccessListener {
+        httpsReference.getFile(localFile).addOnSuccessListener {
             try {
                 // create temp file that will hold byte array
                 //val tempMp3 = File.createTempFile("kurchina", "mp3", cacheDir)
